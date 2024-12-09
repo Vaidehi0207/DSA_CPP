cbccfebd6e4acac3c2910ef976edd331375dee03
Vaidehi0207 <vaidehi.priya02@gmail.com>
2024-12-09 12:39:02 +0530

    Subarray Sum Equals K

---
+++ Mixed/SubarraySumK.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int len = 0;
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += nums[j];
+
+                if(sum == k){
+                    len++;
+                }
+            }
+        }
+        return len;
+
+    }
+};
