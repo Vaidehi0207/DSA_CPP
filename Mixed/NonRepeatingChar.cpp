8b784a4bc20b4de61fda0ae071035aa9efa9c947
Vaidehi0207 <vaidehi.priya02@gmail.com>
2025-01-30 17:12:51 +0530

    done

--- Mixed/NonRepeatingChar.cpp
+++ Mixed/NonRepeatingChar.cpp
@@ -3,29 +3,45 @@ using namespace std;
 
 class Solution {
 public:
-    bool solve(string s, int start, int end){
-        unordered_set<char> mp;
-        for(int i = start; i <= end; i++){
-            if(mp.find(s[i]) != mp.end()){
-                return false;
-            }
-            else{
-                // push that new character into the set
-                mp.insert(s[i]);
-            }
-        }
-        return true;
-    }
+    // bool solve(string s, int start, int end){
+    //     unordered_set<char> mp;
+    //     for(int i = start; i <= end; i++){
+    //         if(mp.find(s[i]) != mp.end()){
+    //             return false;
+    //         }
+    //         else{
+    //             // push that new character into the set
+    //             mp.insert(s[i]);
+    //         }
+    //     }
+    //     return true;
+    // }
     int lengthOfLongestSubstring(string s) {
+        // int n = s.size();
+        // int maxi = 0;
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i; j < n; j++){
+        //         if(solve(s, i, j)){
+        //             maxi = max(maxi, j - i + 1);
+        //         }
+        //     }
+        // }
+        // return maxi;
+
         int n = s.size();
+
+        unordered_set<char> mp;
+        int i = 0;
         int maxi = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(solve(s, i, j)){
-                    maxi = max(maxi, j - i + 1);
-                }
+        for(int j = 0; j < n; j++){
+            while(mp.find(s[j]) != mp.end()){
+                mp.erase(s[i]);
+                i++;
             }
+            mp.insert(s[j]);
+            maxi = max(maxi, j - i + 1);
         }
         return maxi;
+
     }
 };
