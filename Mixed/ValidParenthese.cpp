68bb996a577e687dac9de3d01dce0570b9962160
Vaidehi0207 <vaidehi.priya02@gmail.com>
2025-01-31 23:08:34 +0530

    valid parentheses done

---
+++ Mixed/ValidParenthese.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+
+public:
+    bool checkValidString(string s) {
+        int n = s.size();
+        stack<int> left;
+        stack<int> star;
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                left.push(i);
+            }
+            else if(s[i] == '*'){
+                star.push(i);
+            }
+            else{
+                if(!left.empty()){
+                    left.pop();
+                }
+
+                else if(!star.empty()){
+                    star.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        // check for leftover parentheses 
+        while(!left.empty() &&!star.empty()){
+            if(left.top() > star.top()){
+                return false;
+            }
+            left.pop();
+            star.pop();
+        }
+
+        if(left.empty())
+            return true;
+        return false;
+    }
+};
